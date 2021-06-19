846a559b725464598ab1bba4f650a4d1af57f31e
Godmode <diego.munoz@optivamedia.com>
2021-06-19 12:53:05 +0200

    Add ripple effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
 
-// Add vanilla event to sidebar
+// Add event to sidebar
 document.getElementById('navbarToggler').addEventListener('click', () => {
     document.getElementById('sidebar').classList.toggle('opened');
 })
 
 document.getElementById('overlay').addEventListener('click', () => {
     document.getElementById('sidebar').classList.remove('opened');
+})
+
+// Add ripple effect
+document.querySelectorAll('.ripple').forEach( element => {
+    element.addEventListener('click', (event) => {
+        let x = event.clientX - event.target.offsetLeft;
+        let y = event.clientY - event.target.offsetTop;
+
+        let ripples = document.createElement('span');
+        ripples.style.left = x + 'px';
+        ripples.style.top = y + 'px';
+        ripples.classList += 'ripple__element';
+        console.log(ripples);
+        element.appendChild(ripples);
+
+        
+        setTimeout(()=>{
+            ripples.remove();
+        }, 1000);
+    })
 })
